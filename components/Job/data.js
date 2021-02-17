85ec2b99e7630edf30280238f32d92d8a9d22890
Alex <alex.ayllon@lecreuset.com>
2021-02-17 18:42:08 +0100

    Updated all styles

--- components/Job/data.js
+++ components/Job/data.js
@@ -1,27 +1,32 @@
 export const Data = [
     {
         title: "Oct, 2020 - Current", //Date
-        cardTitle: "Senior Front-End Developer @ Le Creuset",
-        cardSubtitle: "I’m helping Le Creuset to update their entire site for product management from scratch, updating their old look and feel and it tech. (React, Typescript, Sass, Git, Auth0)"
+        cardTitle: "Senior Front-End Developer",
+        cardSubtitle: "Le Creuset",
+        cardDetailedText: "I’m helping Le Creuset to update their entire site for product management from scratch, updating their old look and feel and it tech. (React, Typescript, Sass, Git, Auth0)"
     },
     {
         title: "Dec, 2016 - Oct, 2020", //Date
-        cardTitle: "Full-Stack Developer @ Pasiona Consulting",
-        cardSubtitle: "During my time at Pasiona Consulting, I helped a lot of clients to build the solution they were looking for. Educo: this NGO needed a documental gallery to save and categorize all the images that they had from over the world. (React, .NET, Microsoft Cognitives Services Faces API).         DMG Mori: we achieved to develop a management site to allow them to control all the selling process of their machine tools, from building process to the customer delivery. (React, .NET, Sharepoint Online) Ferrovial: as one of the biggest companies in Spain they had tons of documents stored locally, and we achieved to build a solution to manage all their documents online. (React, .NET, Sharepoint Online) GoTalent: I achieved their requirements building their website usable for totally blind people, getting to achieve AA level accessibility requirements according to WCAG. (HTML, CSS and Javascript Vanilla) Catalana Occidente: I helped updating their entire intranet. (HTML, CSS, Javascript, .NET, MySQL)"
+        cardTitle: "Full-Stack Developer" ,
+        cardSubtitle: "Pasiona Consulting",
+        cardDetailedText: "During my time at Pasiona Consulting, I helped a lot of clients to build the solution they were looking for. Educo: this NGO needed a documental gallery to save and categorize all the images that they had from over the world. (React, .NET, Microsoft Cognitives Services Faces API).         DMG Mori: we achieved to develop a management site to allow them to control all the selling process of their machine tools, from building process to the customer delivery. (React, .NET, Sharepoint Online) Ferrovial: as one of the biggest companies in Spain they had tons of documents stored locally, and we achieved to build a solution to manage all their documents online. (React, .NET, Sharepoint Online) GoTalent: I achieved their requirements building their website usable for totally blind people, getting to achieve AA level accessibility requirements according to WCAG. (HTML, CSS and Javascript Vanilla) Catalana Occidente: I helped updating their entire intranet. (HTML, CSS, Javascript, .NET, MySQL)"
     },
     {
         title: "Aug, 2015 - Nov, 2016", //Date
-        cardTitle: "Full-Stack Developer @ Inbenta",
-        cardSubtitle: "I helped our customers to implement Inbenta’s product, based on Natural Language Processing and semantic search) into their websites. (HTML, CSS, Javascript, PHP)"
+        cardTitle: "Full-Stack Developer",
+        cardSubtitle: "Inbenta",
+        cardDetailedText: "I helped our customers to implement Inbenta’s product, based on Natural Language Processing and semantic search) into their websites. (HTML, CSS, Javascript, PHP)"
     },
     {
         title: "Nov, 2014 - Jul, 2015", //Date
-        cardTitle: "Full-Stack Developer @ Tviso",
-        cardSubtitle: "Their last project (series.ly) was one or the biggest websites in its time, and I helped them building their new plattform (a spanish version of IMDB) (HTML, CSS, Javascript, PHP)"
+        cardTitle: "Full-Stack Developer",
+        cardSubtitle: "Tviso",
+        cardDetailedText: "Their last project (series.ly) was one or the biggest websites in its time, and I helped them building their new plattform (a spanish version of IMDB) (HTML, CSS, Javascript, PHP)"
     },
     {
         title: "Jan, 2014 - Oct, 2014", //Date
-        cardTitle: "Back-End Developer @ The Mamasu Agency",
-        cardSubtitle: "They wanted to build a mobile app with games and prizes, and I helped them with the backend part. (PHP, MySQL)"
+        cardTitle: "Back-End Developer",
+        cardSubtitle: "The Mamasu Agency",
+        cardDetailedText: "They wanted to build a mobile app with games and prizes, and I helped them with the backend part. (PHP, MySQL)"
     },
 ]
